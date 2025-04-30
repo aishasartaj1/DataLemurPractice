b85ede8a6d087224dfb5282746a2b8574af96afb
aishasartaj1 <aishasartaj1@gmail.com>
2025-04-29 20:28:20 -0700

    Add SQL solution: Histogram of Tweets

---
+++ Statistics/histogram-of-tweets.sql
@@ -0,0 +1,11 @@
+-- Histogram of Tweets
+WITH total_tweets AS (
+  SELECT user_id, COUNT(tweet_id) AS tweet_user
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = 2022
+  GROUP BY user_id
+)
+SELECT tweet_user AS tweet_bucket, COUNT(user_id) AS users_num 
+FROM total_tweets
+GROUP BY tweet_user
+ORDER BY tweet_bucket;
